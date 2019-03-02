a366d82c7f1545c2621b2be528e186b8838a8585
Erika Davis <davis2erika@gmail.com>
2019-03-02 22:45:48 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,4 +1,4 @@
-def meal_choice(choice_a, choice_b = "meat")
-  puts "my meal choice is #{choice_a} or #{choice_b}."
+def meal_choice(a = "meat")
+  puts "my meal choice is #{a}."
 end
-meal_choice("vegan")
+meal_choice
